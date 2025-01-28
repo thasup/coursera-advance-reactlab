591a7fffe044699e3a091889848b68fe283a939f
thasup <thanachonfirst@hotmail.com>
2025-01-29 00:14:39 +0700

    task: Finish section 4

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -4,9 +4,13 @@ import {
   AlertDialogContent,
   AlertDialogHeader,
   AlertDialogOverlay,
+  Heading,
+  HStack,
 } from "@chakra-ui/react";
 import { useAlertContext } from "../context/alertContext";
 import { useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
 
 /**
  * This is a global component that uses context to display a global alert message.
@@ -23,9 +27,12 @@ function Alert() {
       onClose={onClose}
     >
       <AlertDialogOverlay>
-        <AlertDialogContent py={4} backgroundColor={isSuccess ? '#81C784' : '#FF8A65'}>
+        <AlertDialogContent margin="auto" py={4} backgroundColor={isSuccess ? '#81C784' : '#FF8A65'}>
           <AlertDialogHeader fontSize="lg" fontWeight="bold">
-            {isSuccess ? 'All good!' : 'Oops!'}
+            <HStack justifyContent="space-between">
+              <Heading size="sm">{isSuccess ? 'All good!' : 'Oops!'}</Heading>
+              <FontAwesomeIcon icon={faClose} size="1x" onClick={onClose} />
+            </HStack>
           </AlertDialogHeader>
           <AlertDialogBody>{message}</AlertDialogBody>
         </AlertDialogContent>
